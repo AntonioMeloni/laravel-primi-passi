dbf8da19fe8c5ff1013f84c7045265ba70e57e64
Antonio <ameloni9@gmail.com>
2020-05-05 09:56:01 +0200

    html lang sostituito

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -142,7 +142,7 @@ foreach ($cards as $key => $card) {
 @endphp
 
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
