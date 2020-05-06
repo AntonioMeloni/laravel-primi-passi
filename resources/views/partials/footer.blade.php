5249efe36f7c00fd209a818ffb7802f4d7a762d0
Antonio <ameloni9@gmail.com>
2020-05-06 19:21:58 +0200

    Footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,59 @@
+<footer>
+    <div class="footer-container">
+        <div class="infos">
+            <img class="logo" src="{{asset('img/marchio-sito-test.png')}}" alt="La Molisana">
+            <p>Ragione sociale: La Molisana S.P.A.</p>
+            <p>Sede legale:Contrada Colle delle Api, 100/A</p>
+            <p>86100 - Campobasso (CB)</p>
+            <p>PEC: lamolisana@pec.it</p>
+            <p>Tel: +39 0874 4981</p>
+            <p>Fax: +39 0874 629584</p>
+            <p>info@lamolisana.it (per segnalazioni degli utenti)</p>
+            <p>commerciale@lamolisana.it</p>
+            <p>export@lamolisana.it</p>
+            <p>Numero verde: 800818081</p>
+            <p>Telefono: 3801292455</p>
+        </div>
+        <div class="footer-links">
+            <div class="footer-list">
+                <h2>PASTIFICIO</h2>
+                <ul>
+                    <li>Il Pastificio</li>
+                    <li>Grano decorticato a pietra</li>
+                    <li>Il Molise c'è</li>
+                    <li>Filiera Integrata</li>
+                    <li>100 anni di pasta</li>
+                    <li>Sartoria della pasta</li>
+                    <li>Spaghetto Quadrato</li>
+                    <li>Le Persone</li>
+                </ul>
+            </div>
+            <div class="footer-list">
+                <h2>COLLEZIONE DA CHEF</h2>
+                <ul>
+                    <li>Collezione da Chef</li>
+                    <li>Grandi Cucine</li>
+                    <li>Biologiche</li>
+                    <li>Quadrate</li>
+                </ul>
+            </div>
+            <div class="footer-list">
+                <h2>PRODOTTI</h2>
+                <ul>
+                    <li>Le Classiche</li>
+                    <li>Le Integrali</li>
+                    <li>Le Speciali</li>
+                    <li>Le Biologiche</li>
+                    <li>Le Gluten-free</li>
+                    <li>Le Semole</li>
+                    <li>Le Extra di Lusso</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-logo">
+        <img src="{{asset('img/footer-montagne-bottom.jpg')}}" alt="">
+    </div>
+
+
+</footer>
